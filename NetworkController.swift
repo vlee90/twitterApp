a373f4df842893ea4818c86511a2dc8137a2a326
Vincent Lee <owner@Owners-MacBook-Air.local>
2014-10-12 07:07:05 -0700

    first commit

---
+++ NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  TwitterApp
+//
+//  Created by Vincent Lee on 10/10/14.
+//  Copyright (c) 2014 VincentLee. All rights reserved.
+//
+
+import Foundation
---
+++ TwitterApp/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  TwitterApp
+//
+//  Created by Vincent Lee on 10/11/14.
+//  Copyright (c) 2014 VincentLee. All rights reserved.
+//
+
+import Foundation
